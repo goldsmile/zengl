c32e21e74ed1858bba27d5326306d7ef38278c34
dr.andru <dr.andru@6573c10b-8653-0410-9706-d32479e959fb>
2012-08-09 19:16:03 +0000

    git-svn-id: http://zengl.googlecode.com/svn/branches/0.3.x@1889 6573c10b-8653-0410-9706-d32479e959fb

--- src/zgl_sound.pas
+++ src/zgl_sound.pas
@@ -1344,7 +1344,7 @@ begin
         end;
     end;
 
-  sfStream[ id ]._decoder.Close( sfStream[ i ] );
+  sfStream[ id ]._decoder.Close( sfStream[ id ] );
   thread_EventDestroy( sfEvent[ id ] );
   EndThread( 0 );
 end;
